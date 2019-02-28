3daa0817d27aff6ab6de118873b41d8dd295acf9
Giovanni Naufal <giovanni.naufal@locaweb.com.br>
2019-02-28 11:05:18 -0300

    Fix redis port

--- lita_config.rb
+++ lita_config.rb
@@ -4,37 +4,20 @@ require './handlers/weather'
 require 'dotenv/load'
 
 Lita.configure do |config|
-  # The name your robot will use.
   config.robot.name = "Marvin"
 
-  # The locale code for the language to use.
-  # config.robot.locale = :en
-
-  # The severity of messages to log. Options are:
-  # :debug, :info, :warn, :error, :fatal
-  # Messages at the selected level and above will be logged.
   config.robot.log_level = :info
 
-  # An array of user IDs that are considered administrators. These users
-  # the ability to add and remove other users from authorization groups.
-  # What is considered a user ID will change depending on which adapter you use.
-  # config.robot.admins = ["1", "2"]
-
-  # The adapter you want to connect with. Make sure you've added the
-  # appropriate gem to the Gemfile.
   config.robot.adapter = :telegram_plus
   config.adapters.telegram_plus.token = ENV['TELEGRAM_TOKEN']
 
-  ## Example: Set options for the chosen adapter.
-  # config.adapter.username = "myname"
-  # config.adapter.password = "secret"
+  if ENV["REDISTOGO_URL"]
+    config.redis[:url] = ENV["REDISTOGO_URL"]
+  else
+    config.redis[:host] = "redis"
+  end
 
-  ## Example: Set options for the Redis connection.
-  config.redis[:host] = ENV["REDISTOGO_URL"]
-  config.http.port = ENV["PORT"]
-  # config.redis.port = 1234
-
-  ## Example: Set configuration for any loaded handlers. See the handler's
-  ## documentation for options.
-  # config.handlers.some_handler.some_config_key = "value"
+  if ENV["PORT"]
+    config.http.port = ENV["PORT"]
+  end
 end
